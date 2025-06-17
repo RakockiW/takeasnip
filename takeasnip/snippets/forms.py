c8322b02b123641b1695ef5e886f2b00933348f5
wiktor.rakocki <wiktor.rakocki6@protonmail.com>
2025-06-18 01:38:04 +0200

    snippet details styling

--- takeasnip/snippets/forms.py
+++ takeasnip/snippets/forms.py
@@ -28,3 +28,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'bg-zinc-900 text-white px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-green-400 border border-transparent hover:border-white transition'
+            })
+        }
